4a9114be3b7b41fb08899b6828c38ac40a126baa
MrPravinS <shegamwarpravin07@gmail.com>
2025-05-10 16:09:42 +0530

    build get and Post API

---
+++ RestAPI/index.js
@@ -0,0 +1,46 @@
+import express from "express"
+import data from './MOCK_DATA.json' with { type: "json" };
+import fs from 'fs'
+import { log } from "console";
+
+
+
+const app = express()
+const PORT = 8000
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+
+app.get("/users",(req,res)=>{
+    const html = `
+    <ul>
+    ${data.map((t)=> `<li>${t.first_name}</li>`).join("")}
+    </ul>
+    `
+    res.send(html)
+})
+app.get("/api/users",(req,res)=>{
+    res.send(data)
+})
+
+app.get("/api/users/:userId",(req,res)=>{
+    const userId = Number(req.params.userId)
+    const user = data.find((u)=> u.id === userId)
+     return res.json(user)
+})
+
+app.post("/api/users",(req,res)=>{
+    const  body = req.body
+    console.log("users", body);
+    
+    data.push({...body, id:data.length + 1});
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(data,null,2),(error)=>{
+      return res.json({status:"success",id:data.length})
+    })
+
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}` );
+    
+})
