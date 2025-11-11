8eafcd5d2ac2f410742b634000deaf6c6f4a2461
MrPravinS <shegamwarpravin07@gmail.com>
2025-11-11 15:03:49 +0530

    monitoring using Morgon and Winston Logs

--- rate_limit/index.js
+++ rate_limit/index.js
@@ -2,9 +2,16 @@ import express from "express";
 import { apiLimiter } from "./middleware/rateLimiter.js";
 import authRoute from "./routes/user.route.js"
 import helmet from "helmet"
+import morgan from "morgan";
+import logger from "./config/logger.js";
 
 const app = express();
 app.set("trust proxy",1)
+app.use(morgan("dev",{
+    stream:{
+        write:(message) => logger.info(message.trim())
+    }
+}))
 
 app.use(express.json());
 app.use(helmet({
@@ -14,6 +21,11 @@ app.use(helmet({
 
 app.use(apiLimiter);
 
+app.get("/",(req,res)=>{
+    logger.info("Home route access")
+    res.send("Hello Backend")
+})
+
 app.use("/api/v1/auth/",authRoute)
 
-app.listen(5000, () => console.log("Server is running on 5000"));
+app.listen(5000, () => logger.info("Server is running on 5000"));
