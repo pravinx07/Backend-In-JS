e05dcb7057f77509751bd71d34edaf1cb5feeac7
MrPravinS <shegamwarpravin07@gmail.com>
2025-11-11 14:40:26 +0530

    added helmet security

--- rate_limit/index.js
+++ rate_limit/index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import { apiLimiter } from "./middleware/rateLimiter.js";
 import authRoute from "./routes/user.route.js"
+import helmet from "helmet"
 
 const app = express();
+app.set("trust proxy",1)
 
 app.use(express.json());
+app.use(helmet({
+    contentSecurityPolicy:false,
+    crossOriginResourcePolicy:{policy:"cross-origin"}
+}))
+
 app.use(apiLimiter);
 
 app.use("/api/v1/auth/",authRoute)
